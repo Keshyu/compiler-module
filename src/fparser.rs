97b95f52536209f552ee4773f044a2aee447819c
Covrick <asdfsdev264@gmail.com>
2020-01-19 16:09:45 +0900

    Create FParser, Update Lexer
    FParser represents another approach to parsing
    Lexer.lex now is supposed to only tell what type of token it just parsed but save parsed token
    Lexer.pop_parsed_token provides access to lastly parsed tokens

---
+++ src/fparser.rs
@@ -0,0 +1,50 @@
+use crate::lexer::Lexer as LexerTrait;
+
+
+pub struct Parser<Lexer, Token, TokenType>
+where
+    Lexer: LexerTrait<Token = Token, TokenType = TokenType>
+{
+    lexer: Lexer,
+}
+
+impl<Lexer, Token, TokenType> Parser<Lexer, Token, TokenType>
+where
+    Lexer: LexerTrait<Token = Token, TokenType = TokenType>
+{
+    pub fn new(source_string: String) -> Self {
+        Parser {
+            lexer: Lexer::new(source_string.into_boxed_str()),
+        }
+    }
+
+    pub fn sequence<Node>(
+        &mut self,
+        token_types: Vec<TokenType>,
+        parse: impl Fn(Vec<Token>) -> Node
+    )
+        -> Node
+    {
+        let parsed_tokens = token_types.into_iter().map(|token_type| {
+            self.lexer.lex(vec![token_type]).expect("Parse Error");
+
+            self.lexer.pop_parsed_token()
+        }).collect();
+
+        parse(parsed_tokens)
+    }
+
+    pub fn choice<Node>(
+        &mut self,
+        token_types: Vec<TokenType>,
+        parse: impl Fn(Token) -> Node,
+    )
+        -> Node
+    {
+        self.lexer.lex(token_types).expect("Parse Error");
+
+        let parsed_token = self.lexer.pop_parsed_token();
+
+        parse(parsed_token)
+    }
+}
--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -4,17 +4,13 @@ mod recorder;
 pub use lex_manager::LexManager;
 pub use recorder::Recorder;
 
-
 pub trait Lexer {
     type Token;
     type TokenType;
 
     fn new(source: Box<str>) -> Self;
 
-    fn lex(
-        &mut self,
-        valid_tokens: Vec<Self::TokenType>,
-        skipable_tokens: Vec<Self::TokenType>,
-    )
-        -> Option<Self::Token>;
+    fn lex(&mut self, valid_tokens: Vec<Self::TokenType>) -> Option<Self::TokenType>;
+
+    fn pop_parsed_token(&mut self) -> Self::Token;
 }
